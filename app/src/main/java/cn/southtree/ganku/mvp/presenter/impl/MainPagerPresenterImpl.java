021dc3cbddce196a6e43f52a1d55be5b3f23c603
rabbitknight <mottchenz@gmail.com>
2018-01-16 19:03:02 +0800

    test

--- app/src/main/java/cn/southtree/ganku/mvp/presenter/impl/MainPagerPresenterImpl.java
+++ app/src/main/java/cn/southtree/ganku/mvp/presenter/impl/MainPagerPresenterImpl.java
@@ -106,6 +106,7 @@ public class MainPagerPresenterImpl extends BasePresenterImpl<MainPagerFragment>
                                 editor.putString("meizi", dataBean.results.get(0).getUrl());
                                 editor.apply();
                             }
+
                             //view.callBack.setMeizi();
                         }
                     }
